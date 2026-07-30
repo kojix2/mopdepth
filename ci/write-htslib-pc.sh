058865d1089bb05af5c73bbc0869777788963c39
kojix2 <2xijok@gmail.com>
2026-07-30 10:39:22 +0900

    Fix pkg-config handling by disabling uninstalled libraries and ensuring correct path resolution for htslib

--- ci/write-htslib-pc.sh
+++ ci/write-htslib-pc.sh
@@ -5,6 +5,8 @@ htslib_dir=${1:-htslib}
 htslib_abs=$(cd "$htslib_dir" && pwd -P)
 version=$(ci/htslib-version.sh "$htslib_dir")
 
+rm -f "$htslib_abs/htslib-uninstalled.pc"
+
 cat > "$htslib_abs/htslib.pc" <<EOF
 includedir=$htslib_abs
 libdir=$htslib_abs
@@ -16,4 +18,9 @@ Cflags: -I\${includedir}
 Libs: -L\${libdir} -lhts
 EOF
 
-PKG_CONFIG_PATH="$htslib_abs:${PKG_CONFIG_PATH:-}" pkg-config --modversion htslib
+PKG_CONFIG_DISABLE_UNINSTALLED=1 PKG_CONFIG_PATH="$htslib_abs:${PKG_CONFIG_PATH:-}" pkg-config --modversion htslib
+pc_path=$(PKG_CONFIG_DISABLE_UNINSTALLED=1 PKG_CONFIG_PATH="$htslib_abs:${PKG_CONFIG_PATH:-}" pkg-config --path htslib)
+test "$pc_path" = "$htslib_abs/htslib.pc" || {
+  echo "Error: pkg-config resolved $pc_path, expected $htslib_abs/htslib.pc" >&2
+  exit 1
+}
